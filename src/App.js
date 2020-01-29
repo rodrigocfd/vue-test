3cf599b0a188c8024bc56a7e2c5a35636fb147fd
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2020-01-29 16:35:42 -0300

    Base project.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Home from './home/Home';
+import './App.css';
+
+function App() {
+	return (
+		<div className="App">
+			Main
+			<Home />
+		</div>
+	);
+}
+
+export default App;
