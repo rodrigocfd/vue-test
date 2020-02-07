41930e6752c463c34a6e01d0ec6b1be22e1c6292
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2020-02-07 17:35:49 -0300

    Menu font styling.

--- src/app/header/Menu.jsx
+++ src/app/header/Menu.jsx
@@ -3,7 +3,14 @@ import styled from 'styled-components';
 
 function Menu() {
 	return (
-		<Div0></Div0>
+		<Div0>
+			<ul>
+				<li>Estruturas Organizacionais</li>
+				<li>Cargos/Funções</li>
+				<li>Relatórios</li>
+				<li>Administração do Sistema</li>
+			</ul>
+		</Div0>
 	);
 }
 
@@ -15,7 +22,28 @@ const Div0 = styled.div`
 	height: 35px;
 	border: 1px solid #88AEA2;
 	border-radius: 7px;
-	background-image: linear-gradient(#f7f7f7, #e3e3e3);
+	background-image: linear-gradient(#f7f7f7, #e2e2e2);
+	color: #44788d;
+
+	> ul {
+		margin: 0;
+		padding: 0;
+		display: flex;
+		flex-direction: row;
+
+		> li {
+			list-style-type: none;
+			flex-basis: 0;
+			flex-grow: 1;
+			text-align: center;
+			line-height: 1.5em;
+			margin-top: 8px;
+			border-right: 1px solid #ccc;
+		}
+		> li:last-child {
+			border-right: 0;
+		}
+	}
 `;
 
 export default Menu;
