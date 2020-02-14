d654752ff01719dc3e2eab2543f254036b852c30
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2020-02-14 14:46:19 -0300

    Stronger security: check auth cookie every route navigation.

--- src/app/Router.jsx
+++ src/app/Router.jsx
@@ -1,28 +1,44 @@
 import React from 'react';
 import {Redirect, Route, Switch} from 'react-router-dom';
 
+import * as Cookie from '../app/cookie';
 import * as Store from '../app/reduxStore';
 import Home from '../home/Home';
 import Login from '../login/Login';
 import NotFound from './NotFound';
 import Texts from '../texts/Texts';
 
+function CookieProtectedRoute({component: Component, ...otherProps}) {
+	const authToken = Cookie.read('authToken');
+	if (authToken === null) { // check cookie state every navigation
+		const update = Store.useUpdate();
+		update('authToken', null);
+	}
+
+	return (
+		<Route {...otherProps} render={props => (
+			authToken !== null // if no auth cookie, go to login
+				? <Component {...props} />
+				: <Redirect to="/login" />
+			)
+		}/>
+	);
+}
+
 function Router() {
 	const authToken = Store.useValue(store => store.authToken);
 
-	if (authToken === null) { // not logged, that's all you'll see
-		return (
+	return authToken === null
+		? ( // not logged, that's all you'll see
 			<Switch>
 				<Route path="/login" component={Login} />
 				<Redirect to="/login" />
 			</Switch>
-		);
-	} else { // logged, show protected stuff
-		return (
+		) : ( // logged, show protected stuff
 			<Switch>
-				<Route path="/home" component={Home} />
-				<Route path="/texts" component={Texts} />
-				<Route path="/404" component={NotFound} />
+				<CookieProtectedRoute path="/home" component={Home} />
+				<CookieProtectedRoute path="/texts" component={Texts} />
+				<CookieProtectedRoute path="/404" component={NotFound} />
 
 				<Route path="/login">
 					<Redirect to="/home" />
@@ -33,7 +49,6 @@ function Router() {
 				<Redirect to="/404" />
 			</Switch>
 		);
-	}
 }
 
 export default Router;
