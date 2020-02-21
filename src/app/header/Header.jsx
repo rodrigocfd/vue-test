bb8d5c841d0f7b9bb25e02afb22113b785a0a351
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2020-02-21 13:53:40 -0300

    Cleaning up unused app assets.

--- src/app/header/Header.jsx
+++ src/app/header/Header.jsx
@@ -1,17 +1,10 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
-import ReduxStore from '../ReduxStore';
 import Menu from './Menu';
 import c from './Header.module.scss';
 
 function Header() {
-	const authToken = ReduxStore.useValue(store => store.authToken);
-
-	if (authToken === null) {
-		return null; // render only when authenticated
-	}
-
 	return (
 		<div className={c.header}>
 			<div className={c.aboveMenu}>
