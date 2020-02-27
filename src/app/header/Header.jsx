ca111baa233e7c11f2a33a0006d5856efe7b7263
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2020-02-27 15:12:22 -0300

    Dynamic loaded user name on header.

--- src/app/header/Header.jsx
+++ src/app/header/Header.jsx
@@ -1,20 +1,39 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
+import useServerRequest from '../useServerRequest';
 import Menu from './Menu';
+import Loading from '../Loading';
 import c from './Header.module.scss';
 
 function Header() {
+	const server = useServerRequest();
+	const [userInfo, setUserInfo] = React.useState(null);
+
+	React.useEffect(() => {
+		server.doGet('/informacaoUsuario')
+			.then(data => setUserInfo(data))
+			.catch(() => {});
+	}, [server]);
+
 	return (
 		<div className={c.header}>
-			<div className={c.aboveMenu}>
+			<div className={c.logoRow}>
 				<div className={c.left}>
 					<div className={c.logo}></div>
 				</div>
-				<div className={c.rite}>
-					<Link to="/first">First</Link> | {' '}
-					<Link to="/second">Second</Link>
-					[Logoff]
+				<div className={c.right}>
+					<div className={c.userName}>
+						{!userInfo
+							? <Loading text="Carregando usuário..." />
+							: <>Olá, {userInfo.nome}</>
+						}
+					</div>
+					<div>
+						<Link to="/first">First</Link> | {' '}
+						<Link to="/second">Second</Link>
+						[Logoff]
+					</div>
 				</div>
 			</div>
 			<Menu />
--- src/first/First.jsx
+++ src/first/First.jsx
@@ -2,29 +2,13 @@ import React from 'react';
 
 import {useModalState} from '../app/modal/Modal';
 import useRefOpt from '../app/useRefOpt';
-import useServerRequest from '../app/useServerRequest';
 import Prompt from './Prompt';
-import Loading from '../app/Loading';
 import c from './First.module.scss';
 
 function First() {
-	const server = useServerRequest();
 	const modalState = useModalState();
 	const nameRef = useRefOpt(null, {focusOnMount: true});
 	const [name, setName] = React.useState('');
-	const [usr, setUsr] = React.useState(null);
-
-	React.useEffect(() => {
-		(async () => {
-			try {
-				const data = await server.doGet('/informacaoUsuario');
-				console.log(data);
-				setTimeout(() => {
-					setUsr(data);
-				}, 1500);
-			} catch (ex) { }
-		})();
-	}, [server]);
 
 	function btnModify() {
 		modalState.open();
@@ -46,14 +30,6 @@ function First() {
 		)}
 		<h1 className={c.title}>First</h1>
 		<h2 className={c.subtitle}>This is the first component.</h2>
-
-		{!usr ? (
-			<div><Loading text="Carregando usuário..." /></div>
-		) : (
-			<h3>{usr.codigo} | {usr.nome}<br />
-				{usr.orgaoUsuario.denominacao}</h3>
-		)}
-
 		<input type="text" ref={nameRef} value={name} onChange={e => setName(e.target.value)} />
 		<input type="button" value="Modify" onClick={btnModify} />
 	</>);
