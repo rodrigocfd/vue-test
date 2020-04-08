f73c0ddb600f0b8645b239a2eeef467774b85ae3
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2020-04-08 19:47:46 -0300

    Basic open/close node.

--- src/app/arvore/No.tsx
+++ src/app/arvore/No.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import useServerGet from 'app/hooks/useServerGet';
 import useServerGetOnMount from 'app/hooks/useServerGetOnMount';
 import Loading from 'app/Loading';
 import UnidadeNoArvore from 'dto/UnidadeNoArvore';
@@ -9,13 +10,36 @@ interface Props {
 }
 
 function No({id}: Props) {
+	const server = useServerGet();
 	const unid = useServerGetOnMount('/unidadeNoArvore?id=' + id) as UnidadeNoArvore;
-	// const unids = useServerGetOnMount('/unidadesNoArvoreFilhas?idPai=' + idOrgao) as UnidadeNoArvore[];
+	const [aberto, setAberto] = React.useState(false);
+
+	function abre() {
+		if (aberto) {
+			setAberto(false);
+		} else {
+			server.doGet('/unidadesNoArvoreFilhas?idPai=' + id)
+				.then((filhas: UnidadeNoArvore[]) => {
+					setAberto(true);
+					console.log(filhas);
+				});
+		}
+	}
 
 	return !unid
 		? <Loading text="Carregando unidade..." />
 		: (
-			<div>{unid.denominacao}</div>
+			<div>
+				{unid.temFilhas &&
+					<span onClick={abre}>[+] </span>
+				}
+				{unid.denominacao}
+				{aberto &&
+					<div>
+						oi
+					</div>
+				}
+			</div>
 		);
 }
 
